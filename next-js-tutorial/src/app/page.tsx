906b41dd6738ae97478316d9dc33097e48614944
Byakuya91 <nganti91@yahoo.com>
2024-06-23 11:33:58 -0500

    I've established the basic links, while learning about layout.tsk and how to have UI components being rendered across all Next.js pages via layout.tsk. Moving onto adding images and styling the page.

--- next-js-tutorial/src/app/page.tsx
+++ next-js-tutorial/src/app/page.tsx
@@ -1,3 +1,13 @@
+// ?Imports
+import Link from "next/link";
+
+// ?TODO:
+// ? 1)Establish the routes and pages(DONE)
+// ? 2)Link between the pages(ONGOING)
+// ? 3)Establish the images for the pages
+// ? 4)Modify the images to fit the page
+// ? 5)clean up the code to keep it DRY(Do NOT Repeat Yourself)
+
 export default function Home() {
   return <div>Home page</div>;
 }
