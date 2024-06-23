ed7f03f5427a9d9f8781a735110c4dda2b6c8889
Byakuya91 <nganti91@yahoo.com>
2024-06-23 10:44:02 -0500

    I've added some routes for my Next.js app and learned how page.tsk files work by being nested inside folders which is defined within the path of the URL and the page.tsk files export a component, displaying the components and JSX on the page.

---
+++ next-js-tutorial/src/app/performance/page.tsx
@@ -0,0 +1,6 @@
+// ! NOTE: Next.js routing will reference the folder and then the "page.tsx" file INSIDE that folder
+// ? http://localhost:3000/performance
+
+export default function PerformancePage() {
+  return <div>Performance Page</div>;
+}
